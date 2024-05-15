1d0ca28c3af1718d7c155f7c0424b887829a2ea9
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-05-15 19:30:15 +0300

    2 sol added 94 &2469

---
+++ 2469. Convert the Temperature/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _2469._Convert_the_Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = ConvertTemperature(36);
+            foreach (var item in temp)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static double[] ConvertTemperature(double celsius)
+        {
+            double[] temps = { celsius + 273.15, (celsius * 1.80) + 32.00 };
+            return temps;
+        }
+
+    }
+}
---
+++ 94. Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _94._Binary_Tree_Inorder_Traversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public class Solution
+    {
+        List<int> ans = new List<int>();
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null) return ans;
+            InorderTraversal(root.left);
+            ans.Add(root.val);
+            InorderTraversal(root.right);
+            return ans;
+
+        }
+    }
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
