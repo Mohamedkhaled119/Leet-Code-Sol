11e21ff7105436423ae5867df7bef4ccfb00aa75
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-05-15 14:53:02 +0300

    Add files via upload
    3110. Score of a String  sol is added

---
+++ 3110. Score of a String/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _3110._Score_of_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ScoreOfString("hello"));
+        }
+        public static  int ScoreOfString(string s)
+        {
+            int sum = 0;
+            char[] chars = s.ToCharArray();
+            for (int i=1;i<chars.Length;i++)
+            {
+                int abs = Math.Abs(chars[i] - chars[i-1]);
+                sum += abs;
+            }
+            return sum;
+
+        }
+    }
+}
