0fbcbae8483f210a6d4913440d1d8ce5cf9bc997
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-06-13 12:34:35 +0300

    two solutions added

---
+++ 1122. Relative Sort Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Immutable;
+
+namespace _1122._Relative_Sort_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = [26, 21, 11, 20, 50, 34, 1, 18];
+            int[] arr2 = [21, 11, 26, 20];
+            var result=RelativeSortArray(arr1, arr2);
+            foreach(int i in result) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            int[]Ans = new int[arr1.Length];
+            int Ans_counter = 0;
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for(int j = 0; j < arr1.Length; j++)
+                {
+                    if (arr2[i] == arr1[j])
+                    {
+                        Ans[Ans_counter] = arr1[j];
+                        arr1[j] = -1;
+                        Ans_counter++;
+                    }
+                }           
+            }
+            Array.Sort(arr1);
+            
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != -1)
+                {
+                    Ans[i]=(arr1[i]);
+                }
+            }
+             return Ans;
+        }
+    }
+}
---
+++ 2037. Minimum Number of Moves to Seat Everyone/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2037._Minimum_Number_of_Moves_to_Seat_Everyone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] seat = [3, 1, 5];
+            int[] studens = [2, 7, 4];
+            Console.WriteLine(MinMovesToSeat(seat,studens));
+        }
+        public static  int MinMovesToSeat(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+            int Moves = 0;
+            
+            
+            for (int i = 0;i<seats.Length;i++)
+            {
+               Moves+=students[i] - seats[i];
+            }
+
+
+
+
+            return Moves;
+        }
+    }
+}
