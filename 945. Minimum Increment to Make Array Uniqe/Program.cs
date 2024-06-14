c7fe773ee729640e8da7d6a9c36f93b0c1d3c9c5
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-06-14 16:14:15 +0300

    solution form problem 945

---
+++ 945. Minimum Increment to Make Array Uniqe/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _945._Minimum_Increment_to_Make_Array_Uniqe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1, 2, 2];
+            Console.WriteLine(MinIncrementForUnique(nums));
+        }
+        public static int MinIncrementForUnique(int[] nums)
+        {
+            int count = 0;
+            Array.Sort (nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] <= nums[i - 1])
+                {
+                    int NewValue =nums[i]+1;
+                    count += NewValue-nums[i-1];
+                    nums[i] = NewValue;
+                    
+                }
+            }
+
+
+            return count;
+
+        }
+    }
+}
