9a0ba8025f41bd3d69413119081667c8f28e1180
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-06-15 03:05:28 +0300

    problem 1689 solution

---
+++ 1689. Partitioning Into Min Number Of Deci-Binary/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _1689._Partitioning_Into_Min_Number_Of_Deci_Binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinPartitions("82734"));
+        }
+        public static int MinPartitions(string n)
+        {
+            int max = 0;
+            foreach (char c in n)
+            {
+                int num = c-'0';
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+    }
+}
