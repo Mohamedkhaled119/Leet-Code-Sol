71781fbb2472d9666b82c2fe7659b226d981b0bd
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-06-15 15:55:01 +0300

    sol of problem 771.jewels and stones

---
+++ 771.jewels and stones/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _771.jewels_and_stones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumJewelsInStones("aA", "AAabbbbb"));
+
+        }
+        public static  int NumJewelsInStones(string jewels, string stones)
+        {
+            int stone_counter = 0;
+            foreach (char jewel in jewels)
+            { 
+                foreach (char stone in stones)
+                {
+                    if (jewel == stone)
+                    {
+                        stone_counter++;
+                    }
+                }
+            }
+            return stone_counter;
+
+
+        }
+    }
+}
