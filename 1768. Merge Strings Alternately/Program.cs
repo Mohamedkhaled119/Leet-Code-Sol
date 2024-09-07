682e3ee57934a3092167a887cdd55a7f01f9cde4
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-09-07 20:02:20 +0300

    new solution added

---
+++ 1768. Merge Strings Alternately/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace _1768._Merge_Strings_Alternately
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MergeAlternately("ab","pqr"));
+        }
+        public static string MergeAlternately(string word1, string word2)
+        {
+            char[] FirstWord=word1.ToCharArray();
+            char[] SecondWord=word2.ToCharArray();
+            int compare = 0;
+            if(FirstWord.Length >= SecondWord.Length)
+            {
+                compare = FirstWord.Length;
+            }
+            else
+            {
+                compare=SecondWord.Length;
+            }
+            string ans = "";
+            for (int i = 0; i < compare; i++)
+            {
+                if (i < FirstWord.Length)
+                {
+                    ans += FirstWord[i];
+                }
+                if (i < SecondWord.Length)
+                {
+                    ans += SecondWord[i];
+                }
+            }
+            return ans;
+
+
+        }
+    
+    }
+}
