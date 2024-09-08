fc17be66147c429555e6fba6f693b78a2ea0335d
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-09-08 11:56:09 +0300

    1071 sol

---
+++ 1071. Greatest Common Divisor of Strings/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _1071._Greatest_Common_Divisor_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GcdOfStrings("ABCDEF","ABC"));
+        }
+
+        public static int GCD(int n1, int n2)
+        {
+            while (n1 != 0 && n2 != 0)
+            {
+                if (n1 > n2)
+                {
+                    n1 %= n2;
+                }
+                else
+                {
+                    n2 %= n1;
+                }
+            }
+
+            return n1 | n2;
+        }
+        public static string GcdOfStrings(string str1, string str2)
+        {
+            if (str1 + str2 != str2 + str1)
+            {
+                return "";
+            }
+
+            int gcdLen = GCD(str1.Length, str2.Length);
+            return str1.Substring(0, gcdLen);
+        }
+    }
+}
