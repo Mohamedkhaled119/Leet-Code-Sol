2ae7a49db633903d78657db222b65b86b0abb024
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-10-20 18:20:52 +0300

    1431 answer

---
+++ 1431.Kids With The Greatest Number Of Candies/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] candies = { 2, 3, 5, 1, 3 };
+            int Extra = 3;
+           IList<bool>x= KidsWithCandies(candies, Extra);
+            foreach(bool i in x)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public  static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+
+            int max = candies.Max();
+            return candies.Select(x => x + extraCandies >= max).ToList<bool>();
+        }
+    }
+}
