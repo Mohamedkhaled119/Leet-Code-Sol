89ed8caab6164e63f0d0102566de0041eaf2f686
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-10-21 17:24:53 +0300

    problem 605 ans

---
+++ 605. can place flowers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _605._can_place_flowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    if ((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                    {
+                        n--;
+                        flowerbed[i] = 1;
+                    }
+                }
+            }
+
+            return n <= 0;
+        }
+
+    }
+}
