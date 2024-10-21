3a5089c2a06f113c04e26dbefaf7dc97fbbcf426
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-10-21 18:11:54 +0300

    problem 345 ans

---
+++ 345.Revers Vowels of a String/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace _345.Revers_Vowels_of_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseVowels("IceCreAm"));
+        
+        }
+        public static string ReverseVowels(string s)
+        {
+            int right = 0;
+            int left = s.Length-1;
+            char[] chars = s.ToCharArray();
+            
+            while(right < left) 
+            {
+                if ("aeiouAEIOU".Contains(chars[right]) && "aeiouAEIOU".Contains(chars[left]))
+                {
+                    char temp = chars[right];
+                    chars[right] = chars[left];
+                    chars[left] = temp;
+                    right++;
+                    left--;
+
+                }
+                if(!"aeiouAEIOU".Contains(chars[right]))
+                {
+                    right++;
+                }
+               if(!"aeiouAEIOU".Contains(chars[left]))
+                {
+                    left--;
+                }
+            }
+            return new string(chars);
+           
+        }
+    }
+}
