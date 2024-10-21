e979ac81a313acf2bef98af4ba56fce1570c8d13
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-10-21 22:22:20 +0300

    2 problems ans added

---
+++ 283.move Zeroes/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace _283.move_Zeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveZeroes([0, 1, 0, 3, 12]);
+            Console.WriteLine("Hello, World!");
+        }
+        public static void MoveZeroes(int[] nums)
+        {
+            int len = nums.Length;
+            int slow = 0;
+            int fast = 0;
+            while (fast < len)
+            {
+                if (nums[fast] != 0)
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+                fast++;
+            }
+            for (int i = slow; i < len; i++) nums[i] = 0;
+        }
+
+    }
+}
---
+++ 392. Is Subsequence/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _392._Is_Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string  s = "bcd", t = "uubcd";
+            Console.WriteLine(IsSubsequence(s,t));
+        }
+        public static bool IsSubsequence(string s, string t)
+        {
+            int first = 0;
+            int second=0;
+            if (s == "")
+            {
+                return true;
+            }
+            while (second < t.Length)
+            {
+                if (s[first] == t[second])
+                {
+                    first++;
+                    second++;
+                    if(first== s.Length)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    second++;
+
+
+                }
+
+            }
+            
+            return false;
+        }
+    }
+}
