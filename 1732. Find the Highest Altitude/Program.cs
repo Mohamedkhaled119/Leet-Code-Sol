6fce27aca8692724e0a1deb8ec2e9023a2c177df
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-10-24 18:52:17 +0300

    problem 1732 solution

---
+++ 1732. Find the Highest Altitude/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _1732._Find_the_Highest_Altitude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(LargestAltitude([-5, 1, 5, 0, -7]));
+        }
+        public static int LargestAltitude(int[] gain)
+        {
+            List<int> result = new List<int>();
+            result.Add(0);
+            for (int i = 0; i < gain.Length; i++)
+            {
+                 result.Add( result[i]  + gain[i]);
+
+            }
+            return result.Max(x=>x);
+
+        }
+    }
+}
